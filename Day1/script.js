7cb9cb734a269abea1d5ccbc4efd0b99a22cc323
Soul Prime <rg8947930@gmail.com>
2026-07-31 10:45:27 +0530

    class 3

---
+++ Day1/script.js
@@ -0,0 +1,29 @@
+// const app=require("./app.js");
+
+
+// var x=10;
+// var y=20;
+// var z=30;
+// console.log(x+y+z);
+
+
+// var x=parseInt(prompt("Enter a number:"));
+// if(x==20){
+//     console.log("x is 20");
+// }
+// else {console.log("lets goooo! You are very wrong");}
+
+
+// for(var i=0; i<10; i++){
+//     console.log(i);
+// }
+
+
+// var x=20;
+// const arr=[1,2,3,4,5];
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+// console.log(app.xz);
