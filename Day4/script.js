09b90570420fa3e371c829e09456b308179da924
Soul Prime <rg8947930@gmail.com>
2026-08-19 14:05:16 +0530

    lets go

---
+++ Day4/script.js
@@ -0,0 +1,18 @@
+console.log("====String Methods====");
+
+let name="Romit";
+console.log(name);
+console.log(name.length);
+
+let str="Welcome Javascript. Lets learn more about Javascript";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charAt(3));
+console.log(str.indexOf("Java"));
+console.log(str.lastIndexOf("Java"));
+console.log(str.slice(0,20));
+console.log(str.split("."));
+console.log(str.replace("Javascript","Java"));
+console.log(str.replaceAll("Javascript","Java"));
+name=name.concat(" ","Gupta"); console.log(name);
+console.log(str.includes("Python")); // return true or false after checking the presecence
---
+++ Day4/script2.js
@@ -0,0 +1,19 @@
+console.log("====Array Methods====");
+let numbers=[10,20,30,40,50];
+console.log(numbers);
+
+numbers.push(60); console.log(numbers);
+numbers.pop(); console.log(numbers);
+numbers.unshift(0); console.log(numbers); // push from front
+numbers.shift(); console.log(numbers); // pop from front
+
+
+console.log("====Date Object====");
+let date=new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+
+
+console.log("====Math Object====");
